2b52b518b651ed0aeb098793dda7af6fe3963c6e
Nguyen Quoc Hung <nguyenquochung.workvn@gmail.com>
2023-09-20 11:11:39 +0700

    Update App.js

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
-
-function Button() {
-  const [color, setColor] = useState("red");
-  const newColor = color === "red" ? "blue" : "red";
-  return <button style={{ backgroundColor: color }} onClick={() => setColor(newColor)}>Change to {newColor}</button>
-}
-
-
 
 export default App;
